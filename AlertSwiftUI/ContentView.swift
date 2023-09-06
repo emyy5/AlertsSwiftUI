9ef9fcbb71928081947804a34082aec0968f8c36
Github <emyy5>
2023-09-06 08:24:17 +0300

    first alert

--- AlertSwiftUI/ContentView.swift
+++ AlertSwiftUI/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alertIsPressed = false
+    @State private var backgroundUpdate = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            ZStack{
+                if backgroundUpdate {
+                    Color.red
+                }
+                else{
+                    Color.blue
+                }
+                VStack {
+                    Button(action:{
+                        self.alertIsPressed = true
+                        
+                    } , label:{ Text("Tab me!")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    })
+                    
+                    .frame(width: 200, height: 50, alignment: .center)
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(8)
+                    .alert(isPresented: $alertIsPressed, content: {
+                        Alert(title: Text("would you like to Purchase?"), primaryButton: .default(Text("Got it"),
+                                                                                                  action: {
+                            //print("start purchase")
+                            self.backgroundUpdate.toggle()
+                            
+                        }),
+                              secondaryButton: .cancel(Text("No thanks")))
+                    })
+                }
+            }
+            .navigationTitle("SwiftUi Alerts")
+            
         }
-        .padding()
     }
 }
 
