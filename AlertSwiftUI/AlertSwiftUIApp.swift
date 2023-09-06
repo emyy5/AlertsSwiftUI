e47f127388b224e47c4a7aa301eea0133ecdf00e
Github <emyy5>
2023-09-06 07:54:03 +0300

    Initial Commit

---
+++ AlertSwiftUI/AlertSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSwiftUIApp.swift
+//  AlertSwiftUI
+//
+//  Created by Eman Khaled on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlertSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
